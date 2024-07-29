1841f62ffff0350e15bd965e73123d2a4870819b
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2024-07-29 16:38:24 -0400

    Data cleaning.

--- Team_Project.R
+++ Team_Project.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 # Import data.
 raw_data <- read.csv("project_data.csv")
 View(raw_data)
@@ -12,8 +14,6 @@ names(raw_data)
 working_data <- raw_data[,c(1,2,3,9,16,18,19,20,24,42,46,58,70,75,77,85,86,87)]
 # No blanks in data set.
 which(working_data == "")
-summary(working_data)
-str(working_data)
 
 # Count NAs in each variable.
 for (i in names(working_data)) {
@@ -26,4 +26,28 @@ clean_data <- na.omit(working_data)
 summary(clean_data)
 str(clean_data)
 
-# Make logicals, scales into factors.
+# Rename variables.
+clean_data1 <- clean_data %>%
+  rename(TFT = "Time.from.transplant", 
+         Liver.diag = "Liver.Diagnosis",
+         RoD = "Recurrence.of.disease",
+         RGD = "Rejection.graft.dysfunction",
+         Any.fibro = "Any.fibrosis",
+         Renal.fail = "Renal.Failure",
+         ESS = "Epworth.Sleepiness.Scale",
+         PSQIS = "Pittsburgh.Sleep.Quality.Index.Score",
+         AIS = "Athens.Insomnia.Scale",
+         BSS = "Berlin.Sleepiness.Scale")
+
+# Convert sleep disturbance measurements to binary.
+clean_data2 <- clean_data1 %>%
+  mutate(ESS = ifelse(ESS > 10, 1, 0)) %>%
+  mutate(PSQIS = ifelse(PSQIS > 4, 1, 0)) %>%
+  mutate(AIS = ifelse(AIS > 5, 1, 0))
+
+# Change categorical variables to factors.
+clean_data3 <- clean_data2 %>% 
+  mutate(across(c("Gender", "Liver.diag", "RoD", "RGD", "Any.fibro",
+                  "Renal.fail", "Depression", "Corticoid", "ESS", 
+                  "PSQIS", "AIS", "BSS"), as.factor))
+
